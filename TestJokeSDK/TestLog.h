3df6be5926ea4572de3214b439b3b340bd8c1131
iMac <admin@iMacdeiMac.local>
2018-06-04 17:53:04 +0800

    as

---
+++ TestJokeSDK/TestLog.h
@@ -0,0 +1,13 @@
+//
+//  TestLog.h
+//  TestJokeSDK
+//
+//  Created by iMac on 2018/6/4.
+//  Copyright © 2018年 iMac. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestLog : NSObject
++ (void)testLog;
+@end
---
+++ TestJokeSDK/TestLog.m
@@ -0,0 +1,15 @@
+//
+//  TestLog.m
+//  TestJokeSDK
+//
+//  Created by iMac on 2018/6/4.
+//  Copyright © 2018年 iMac. All rights reserved.
+//
+
+#import "TestLog.h"
+
+@implementation TestLog
++ (void)testLog {
+    NSLog(@"test log...");
+}
+@end
