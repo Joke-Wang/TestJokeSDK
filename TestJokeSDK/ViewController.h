ea101c6e043210a6c3a459791d7a1930da279d79
iMac <admin@iMacdeiMac.local>
2018-06-04 17:50:21 +0800

    add all

---
+++ TestJokeSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestJokeSDK
+//
+//  Created by iMac on 2018/6/4.
+//  Copyright © 2018年 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestJokeSDK/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestJokeSDK
+//
+//  Created by iMac on 2018/6/4.
+//  Copyright © 2018年 iMac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
